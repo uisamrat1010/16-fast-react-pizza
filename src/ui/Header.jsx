d6e19df38576844910cf7def15efaab3cdf01a59
uisamrat1010 <uisamratc@gmail.com>
2024-09-21 13:18:46 +0530

    update logo

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/searchOrder';
 import Username from '../features/user/Username';
+import Logo from '../fastreactlogo.png';
 
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fast React Pizza.Co{' '}
+        <img src={Logo} alt="logo" className="h-auto w-96" />
       </Link>
       <SearchOrder />
       <Username />
